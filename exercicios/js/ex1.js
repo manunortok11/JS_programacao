cef1f0cfd1e1d10905921e616cadbc3265b9cf8f
manunortok11 <emanuelinortokk@gmail.com>
2025-08-27 16:14:00 -0300

    exercicios
    ola

--- exercicios/js/ex1.js
+++ exercicios/js/ex1.js
@@ -1,6 +1,10 @@
-var numero
+var numero, triplo
 numero=parseInt(prompt("Digite um numero: "))
 
 if(numero<=3){
-    
+ triplo=numero*3
+ alert("O triplo é: "+triplo)
+
+}else{
+    alert("Digite outro numero.")
 }
