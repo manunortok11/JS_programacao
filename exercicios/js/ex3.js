9d5a3868f1acc71e4719a8a55b09af3bc5fa7026
manunortok11 <emanuelinortokk@gmail.com>
2025-09-02 19:17:30 -0300

    ex3finalizado

--- exercicios/js/ex3.js
+++ exercicios/js/ex3.js
@@ -1,14 +1,18 @@
-var n1, n2, n3, n4, media
+var n1, n2, n3, n4, media, freq
 
 n1=parseInt(prompt("Digite sua nota 1: "))
 n2=parseInt(prompt("Digite sua nota 2: "))
 n3=parseInt(prompt("Digite sua nota 3: "))
 n4=parseInt(prompt("Digite sua nota 4: "))
+freq=parseInt(prompt("Digite seu percentual de frequencia: "))
 
 media=(n1+n2+n3+n4)/4
-if(media>7){
-    alert("Sua média foi maior ou igual a 7, portanto você foi aprovado")
+if(media>7 && freq>75){
+    alert("Sua média foi maior ou igual a 7, e sua frequencia superior a 75% portanto você foi aprovado")
 }
-if(media<7){
-    alert("Sua média nao foi suficiente, está reprovado.")
+if(media<7 && freq<75){
+    alert("Sua média nao foi suficiente, e sua frequencia também, você está reprovado.")
+}
+if(media>=7 && freq<75){
+    alert("Você deve realizar o exame para ser aprovado.")
 }
